67c6ac2624e5071010286c40552b86ad90b07bb5
jeremy <jeremyfdomingo@gmail.com>
2024-11-14 21:31:40 -0600

    Implemented ransackable associations/attributes for Status and User models.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,11 @@
 class Status < ApplicationRecord
   has_many :orders
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "orders" ]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "id", "status", "updated_at" ]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :orders
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "orders" ]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "address", "created_at", "email", "first_name", "id", "last_name", "password", "phone_number", "updated_at" ]
+  end
 end
