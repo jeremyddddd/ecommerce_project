006a21a8fd714657e176a4f9f6b42b152f918820
jeremy <jeremyfdomingo@gmail.com>
2024-11-14 22:26:30 -0600

    Implemented a category drop-down filtering within the product page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.includes(:image_attachment, :image_blob, :category)
+    @categories = Category.all
+    if params[:category_id].present?
+      @products = Product.where(category_id: params[:category_id]).includes(:image_attachment, :category)
+    else
+      @products = Product.includes(:image_attachment, :category)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root "products#index"  # Set the homepage to display all products
+  root "products#index"
   resources :products, only: [ :index ]
 
+  resources :categories, only: [] do
+    resources :products, only: :index
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
